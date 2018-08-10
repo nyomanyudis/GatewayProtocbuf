7f3480607eb94f2feb51b7eb5c5cd2550a02b778
eantaru <reyhan@CiptaCOLDSPOT.local>
2018-08-10 08:51:04 +0700

    First Commit

---
+++ GatewayProtocbuf/GatewayProtocbuf.h
@@ -0,0 +1,15 @@
+//
+//  GatewayProtocbuf.h
+//  GatewayProtocbuf
+//
+//  Created by Reyhan on 7/15/13.
+//  Copyright (c) 2013 Reyhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GatewayProtocbuf : NSObject
+
+- (NSString *) hello;
+
+@end
---
+++ GatewayProtocbuf/GatewayProtocbuf.m
@@ -0,0 +1,18 @@
+//
+//  GatewayProtocbuf.m
+//  GatewayProtocbuf
+//
+//  Created by Reyhan on 7/15/13.
+//  Copyright (c) 2013 Reyhan. All rights reserved.
+//
+
+#import "GatewayProtocbuf.h"
+
+@implementation GatewayProtocbuf
+
+- (NSString *) hello
+{
+    return @"Hello from GatewayProtocbuf in GatewayProtocbuf project";
+}
+
+@end
